d8365affe1fb7e28aff5f784ad273694914bc695
babulanas51 <babulanas51@gmail.com>
2025-10-15 12:09:46 +0700

    Add files via upload
    tugas python

---
+++ ini python.py
@@ -0,0 +1,10 @@
+panjang = int(input("masukan panjang ="))
+lebar = int (input("masukan lebar :"))
+pengurangan = int(panjang)-int(lebar)
+luas = int (panjang)*int(lebar)
+keliling = int(panjang)+int(lebar)+int(panjang)+int(lebar)
+print(f"panjang {panjang} dikurangi lebar {lebar} adalah {pengurangan}" )
+
+print(f"panjang {panjang} cm dan lebar {lebar}cm maka luas persegi panjang adalah{luas} cm" )
+
+print(f"panjang {panjang} cm dan lebar {lebar}cm maka keliling persegi panjang adalah {keliling} cm" )
